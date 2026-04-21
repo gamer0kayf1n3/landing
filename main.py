9e615dd94079a395304d004267974f207fe2fd0b
gamer0kayf1n3 <muser0943@gmail.com>
2026-04-21 15:18:36 +0800

    compiled

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from flask import Flask, send_from_directory
 app = Flask(__name__)
+PATH = "/home/ubuntu/landing/hodgkraft-landing/dist"
 @app.route("/")
 def main():
-    return send_from_directory("/home/ubuntu/landing", "index.html")
+    return send_from_directory(PATH, "index.html")
+@app.route("/landing/<path:file>")
+def files(file):
+    return send_from_directory(PATH, file)
 app.run(host="0.0.0.0", port=8080)
