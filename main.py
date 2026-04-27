9aa187a6c096a3aeca2122ec3d7479239739a997
gamer0kayf1n3 <muser0943@gmail.com>
2026-04-27 22:07:50 +0800

    completely moved crafty away from main domain; moved /landing/ to /

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from flask import Flask, send_from_directory
+
 app = Flask(__name__)
 PATH = "/home/ubuntu/landing/hodgkraft-landing/dist"
+
 @app.route("/")
 def main():
     return send_from_directory(PATH, "index.html")
-@app.route("/landing/<path:file>")
+
+@app.route("/<path:file>")
 def files(file):
     return send_from_directory(PATH, file)
-
 
 @app.route("/robots.txt")
 def robotstxt():
     return send_from_directory("/home/ubuntu/landing", "robots.txt")
+
 app.run(host="0.0.0.0", port=8080)
